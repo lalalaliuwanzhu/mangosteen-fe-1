38c3f7b8e6ca183570bb48520cf7f7d5415af334
liuwanzhu <992536725@qq.com>
2023-07-15 18:05:49 +0800

    tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { defineComponent, DefineComponent, ref } from "vue";
 import { RouterView } from 'vue-router'
 import './App.scss'
+import 'vant/lib/index.css';
 
 export const App = defineComponent({
   setup() {
